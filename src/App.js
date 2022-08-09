e5cdf52a8e2f7d90e5cb9a19f987599833c56a5d
Salih Komut <salih.komut@student.ap.be>
2022-08-10 01:51:36 +0200

    eerste stappen om dynamisch films te tonen

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from "react";
 import './App.css';
-import SearchIcon from './search.svg';
+import SearchIcon from './search.svg'
+import MovieCard from './MovieCard';
 import { useEffect } from "react"; 
+import { useState } from "react";
 
 // 68afb008
 
@@ -18,11 +20,13 @@ const movie1=
 
 const App = () => {
 
+    const [movies, setMovies] = useState([]);
+
     const searchMovies = async (title) => {
         const response = await fetch(`${API_URL}&s=${title}`)
         const data = await response.json();
 
-        console.log(data.Search);
+        setMovies(data.Search);
     }
 
     useEffect(() => {
@@ -46,23 +50,22 @@ const App = () => {
                 />
             </div>
 
-            <div className="container">
-                <div className="movie">
-                    <div>
-                        <p>{movie1.Year}</p>
-                    </div>
+            {
+                movies?.length > 0
+                 ? (
+                        <div className="container">
+                            <div className="movie">
+                                <MovieCard movie1 = {movie1}/>
+                            </div>
+                        </div>
+                    ) : (
+                        <div className="empty">
+                            <h2>No movies found</h2>
+                        </div>
+                    )
+            }
 
-                    <div>
-                        <img src={movie1.Poster !== 'N/A' ? movie1.Poster: 'https://via.placeholder.com/400'} alt={movie1.Title} />
-                    </div>
-
-                    <div>
-                        <span>{movie1.Type}</span>
-                        <h3>{movie1.Title}</h3>
-                    </div>
-
-                </div>
-            </div>
+            
         </div>
     );
 }
