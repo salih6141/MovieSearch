e5981c0489d56b3e81dce732818eb2a21459b9ba
Salih Komut <salih.komut@student.ap.be>
2022-08-10 01:27:06 +0200

    styling plus api call test

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
 import React from "react";
+import { useEffect } from "react"; 
+
+// 68afb008
+
+const API_URL = 'http://www.omdbapi.com?apikey=68afb008';
 
 const App = () => {
+
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`)
+        const data = await response.json();
+
+        console.log(data.Search);
+    }
+
+    useEffect(() => {
+        	searchMovies('spiderman');
+    }, []);
+
     return(
             <h1>Hello World</h1>
     );
