19ed733a6c7b0be2a05fb5e450ec9d6ee939e96f
Salih Komut <salih.komut@student.ap.be>
2022-08-10 02:08:12 +0200

    search functionaliteit toegevoegd met usestate voor searchterm en callback naar searchterm in onclick functie van searchbutton

--- src/App.js
+++ src/App.js
@@ -21,6 +21,7 @@ const movie1=
 const App = () => {
 
     const [movies, setMovies] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('');
 
     const searchMovies = async (title) => {
         const response = await fetch(`${API_URL}&s=${title}`)
@@ -40,29 +41,28 @@ const App = () => {
             <div className="search">
                 <input
                     placeholder="Search for movies"
-                    value="Superman"
-                    onChange={() => {}}
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
                 />
                 <img
                     src={SearchIcon}
                     alt="search"
-                    onClick={() => {}}
+                    onClick={() => searchMovies(searchTerm)}
                 />
             </div>
 
-            {
-                movies?.length > 0
-                 ? (
-                        <div className="container">
-                                {movies.map((movie) => (
-                                    <MovieCard movie={movie}/>
-                                ))}
-                        </div>
-                    ) : (
-                        <div className="empty">
-                            <h2>No movies found</h2>
-                        </div>
-                    )
+            {movies?.length > 0
+                ? (
+                    <div className="container">
+                        {movies.map((movie) => (
+                            <MovieCard movie={movie}/>
+                        ))}
+                    </div>
+                ) : (
+                    <div className="empty">
+                        <h2>No movies found</h2>
+                    </div>
+                )
             }
 
             
